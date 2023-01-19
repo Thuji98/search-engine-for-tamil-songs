8eb8a2c3d2efce668164b2d1e58ff903477c5dc6
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2023-01-19 14:32:19 +0530

    Updated

--- app.py
+++ app.py
@@ -10,16 +10,49 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/search', methods=['GET','POST'])
-def hello_world():
+@app.route('/search_all', methods=['GET','POST'])
+def search_all():
     if request.method == 'POST':
         query = request.form['searchTerm']
-        res = search(query)
+       
+
+        res = search([0,query])
+
+
         hits = res['hits']['hits']
         time = res['took']
         num_results = res['hits']['total']['value']
 
         return render_template('search_results.html', query=query, hits=hits, num_results=num_results, time=time)
+
+    if request.method == 'GET':
+        return render_template('index.html', init='True')
+
+
+@app.route('/search_target', methods=['GET','POST'])
+def search_target():
+    if request.method == 'POST':
+        target = request.form['target']
+        res = search([1,target])
+        hits = res['hits']['hits']
+        time = res['took']
+        num_results = res['hits']['total']['value']
+
+        return render_template('search_results.html', query=target, hits=hits, num_results=num_results, time=time)
+
+    if request.method == 'GET':
+        return render_template('index.html', init='True')
+
+@app.route('/search_term', methods=['GET','POST'])
+def search_term():
+    if request.method == 'POST':
+        term = request.form['term']
+        res = search([2,term])
+        hits = res['hits']['hits']
+        time = res['took']
+        num_results = res['hits']['total']['value']
+
+        return render_template('search_results.html', query=term, hits=hits, num_results=num_results, time=time)
 
     if request.method == 'GET':
         return render_template('index.html', init='True')
--- query.py
+++ query.py
@@ -3,32 +3,55 @@ from elasticsearch import Elasticsearch
 client = Elasticsearch(HOST="http://localhost", PORT=9200)
 INDEX = 'lyrics_metaphor_db'
 
-def search(query):
-    query_body = process_query(query)
+def search(list_):
+    query_body = process_query(list_)
     response = client.search(index=INDEX, body=query_body)
     return response
 
-def process_query(query):
-    query_body = basic_search(query)
+def process_query(list_):
+    if (list_[0] == 1):
+        query_body = multi_match_query(list_[1])
+    elif (list_[0] == 2):
+        query_body = term_query(list_[1])
+    else:
+        query_body = basic_search(list_[1])
     return query_body
 
+
+# Basic Search Query
 def basic_search(query):
-    query = {
+    q = {
         "query": {
             "query_string": {
                 "query": query
             }
         }
     }
-    return query
+    return q
 
-def exact_search(query):
+# Multi Match Query
+def multi_match_query(target):
+    q = {
+            "query": {
+                "multi_match" : {
+                    "query": target,
+                    "type": "most_fields",
+                    "fields": [ "இலக்கு_1", "இலக்கு_2", "இலக்கு_3", "இலக்கு_4", "இலக்கு_5", "இலக்கு_6"]
+                }
+            }
+        }
+    return q
+
+# Term query
+def term_query(query):
     q = {
         "query": {
-            "multi_match": {
-                "query": query,
-                "type": "phrase"
+            "term": {
+                "வருடம்": {
+                    "value": query
+                }
             }
         }
     }
     return q
+
